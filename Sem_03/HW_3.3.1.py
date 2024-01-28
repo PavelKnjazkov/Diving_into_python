c053bf6ad767e5a1aa022f6e6cc5dfba4b806ff0
PaulFritz <knjazkov.pavel@mail.ru>
2024-01-29 00:56:19 +0300

    Добавил дз3 сем3 (2-ва варианта)

---
+++ Sem_03/HW_3.3.1.py
@@ -0,0 +1,30 @@
+# 3. Создайте словарь со списком вещей для похода в качестве
+# ключа и их массой в качестве значения. Определите какие
+# вещи влезут в рюкзак передав его максимальную
+# грузоподъёмность. Достаточно вернуть один допустимый вариант.
+# *Верните все возможные варианты комплектации рюкзака.
+
+from random import randint
+
+def variants_generator(thigs: list) -> dict:
+     result_dict = {}    
+     for thig in thigs:
+        result_dict[thig] = randint(1, 5)
+     return result_dict
+
+list_things = ['ключи', 'кошелек', 'телефон', 'зажигалка']
+
+items = variants_generator(list_things)
+
+max_weight = 10
+current_weight = 0
+backpack = {} 
+
+for item, weight in items.items():
+    if current_weight + weight <= max_weight:
+        backpack[item] = weight
+        current_weight += weight
+        
+print(f'В рюкзак вместительностью {max_weight} кг вошли следующие вещи:')
+for item, weight in backpack.items():
+    print(f'- {item} - {weight} кг')
---
+++ Sem_03/HW_3.3.py
@@ -0,0 +1,42 @@
+# 3. Создайте словарь со списком вещей для похода в качестве
+# ключа и их массой в качестве значения. Определите какие
+# вещи влезут в рюкзак передав его максимальную
+# грузоподъёмность. Достаточно вернуть один допустимый вариант.
+# *Верните все возможные варианты комплектации рюкзака.
+
+def backpack_options(weight_limit: int, things_value: dict) -> list:
+    """
+    Предлагает варианты комплектации рюкзака, в зависимости от макс. веса
+    :param weight_limit: лимит загрузки рюкзака
+    :param things_value: список вещей
+    :return: возможные варианты комплектации
+    """
+    return_list = []
+    len_name = 0
+    for i in things_value:
+        len_name += len(i) + 2
+    n_variant = 1
+    for i in range(1, (2 ** len(things_value))):
+        str_backpack = ''
+        bin_status = bin(i).rjust(len(things_value) + 2, '0')
+        bin_status = bin_status.replace('0b', '')
+        weight = 0
+        for j, item in enumerate(things_value):
+            if bin_status[j] == '1':
+                str_backpack += f' {item},'
+                weight += things_value[item]
+        if len(str_backpack) > 0:
+            str_backpack = str_backpack[:-1]
+        if weight <= weight_limit:
+            str_backpack = f'Вариант {n_variant: <1}  ' + f'{str_backpack: <{len_name}}' + f' Вес: {weight: >1}\r'
+            n_variant += 1
+            return_list.append(str_backpack)
+    return return_list
+
+
+BACKPACK_WEIGHT = 1.0
+things = dict(ключи=0.3, кошелек=0.2, телефон=0.5, зажигалка=0.1)
+
+list_variant = backpack_options(weight_limit=BACKPACK_WEIGHT, things_value=things)
+for variant in list_variant:
+    print(variant)
