4419d6d9b182023659c093c254716f8b4a8a311a
PaulFritz <knjazkov.pavel@mail.ru>
2024-02-07 01:03:29 +0300

    Добавил решение зад 3 к сем 5

---
+++ Sem_05/HW_5.3.py
@@ -0,0 +1,45 @@
+# Создайте функцию генератор чисел Фибоначчи (см. Википедию).
+
+def gen_fibonacci(step: int):
+    """
+    Генератор чисел Фибоначчи.
+
+    :param step: количество шагов генератора
+    """
+    value_1 = 0
+    value_2 = 1
+    value_result = None
+
+    for i in range(step):
+        match i:
+            case 0:
+                value_result = 0
+            case 1:
+                value_result = 1
+            case _:
+                value_result = value_1 + value_2
+                value_1 = value_2
+                value_2 = value_result
+        yield value_result
+
+
+for item in gen_fibonacci(20):
+    print(item, end=' ')
+
+
+
+# 3. Создайте функцию генератор чисел Фибоначчи (см. Википедию).
+
+# def fibonacci():
+#     a, b = 0, 1
+#     while True:
+#         yield a
+#         a, b = b, a + b
+       
+
+# SIZE = 10
+
+# fib = iter(fibonacci())
+
+# for _ in range(SIZE):
+#     print(next(fib))
